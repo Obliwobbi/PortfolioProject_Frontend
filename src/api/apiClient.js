85f4f9b6af6c4c382e73fbc6860c7bbe37c5a9c6
Obliwobbi <dev@obli.dk>
2026-06-05 11:38:10 +0200

    deployed

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,5 +1,4 @@
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-const API_BASE_URL = 'http://localhost:7070/api/v1'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 async function handleResponse(response) {
   if (!response.ok) {
